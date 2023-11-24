ab1cc20d74ddd2a502671e53f39d70185e0bbc88
btonasse <33516406+btonasse@users.noreply.github.com>
2023-11-24 11:18:03 +0100

    add null handler to top level

--- aiopulse/__init__.py
+++ aiopulse/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 from .core.factory import RequestFactory
 from .core.queue import RequestQueue
 from .core.request import Request
@@ -5,3 +7,6 @@ from .core.response import ProcessedResponse
 from .core.schema import GenericInputSchema, InputSchemaBase
 from .core.session import Session
 from .core.transformer import BaseURLTransformer, GenericTransformer, TransformerBase
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
